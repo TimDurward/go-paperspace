10a433e42f4f0466e5612228d46617fab5235de2
Tim Durward <timothy.a.durward@gmail.com>
2018-11-09 22:46:59 -0800

    Implement base Paperspace client.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # go-paperspace
-Go client for Paperspace
+
+go-paperspace is a Go client library for accessing the Paperspace API.
+
+You can view Paperspace API docs here: https://www.paperspace.com/api
+
+# 🔨 WIP 🔧
+### Done:
+1. Paperspace Client
+2. Base http Requests and Response handling.
+3. Scripts methods semi complete.
+
+### Not Done:
+1. Machine methods
+2. Network methods
+3. Project methods
+4. Jobs methods
+5. Tests
+6. Versioning
+
+# Contributing
+#### Feel free to submit a Github issue in regards to questions.
+
+If you submit a pull request, please keep the following guidelines in mind:
+
+1. Code should be `go fmt` compliant.
+2. Types, structs and funcs should be documented.
+
+## Getting set up
+
+Assuming your `$GOPATH` is set up according to your desires, run:
+
+```sh
+go get github.com/timdurward/go-paperspace
+```
---
+++ paperspace.go
@@ -0,0 +1,161 @@
+package paperspace
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+const (
+	clientVersion  = "0.1.0"
+	defaultBaseURL = "https://api.paperspace.io/"
+	userAgent      = "paperspace/" + clientVersion
+	mediaType      = "application/json"
+
+	headerRateLimit     = "RateLimit-Limit"
+	headerRateRemaining = "RateLimit-Remaining"
+	headerRateReset     = "RateLimit-Reset"
+)
+
+// Config manages Authorization with Paperspace API.
+type Config struct {
+	// API Key for client authorization.
+	APIKey string
+}
+
+// Client manages communication with Paperspace API.
+type Client struct {
+	// HTTP client used to communicate with the API.
+	client *http.Client
+
+	// Base URL for API requests.
+	BaseURL *url.URL
+
+	// UserAgent for client.
+	UserAgent string
+
+	// Config for client.
+	Config *Config
+
+	// Services used for talking to different parts of the Paperspace API.
+	Scripts *ScriptsService
+}
+
+// Response is a Paperspace response. This wraps the standard http.Response returned from Paperspace.
+type Response struct {
+	*http.Response
+}
+
+// ErrorResponse reports the error caused by an API request
+type ErrorResponse struct {
+	// HTTP response that caused this error
+	Response *http.Response
+
+	// Error message
+	Message string `json:"message"`
+}
+
+// NewClient returns a new Paperspace API client.
+func NewClient(config *Config, httpClient *http.Client) (*Client, error) {
+	// If client is not provided (nil) pass in default HTTP client.
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+
+	// Parse defaultBaseURL into URL structure.
+	parsedBaseURL, err := url.Parse(defaultBaseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Client{
+		client:    http.DefaultClient,
+		BaseURL:   parsedBaseURL,
+		UserAgent: userAgent,
+		Config:    config,
+	}
+	c.Scripts = &ScriptsService{client: c}
+
+	return c, nil
+}
+
+// NewRequest creates a new Paperspace API request.
+// A relative URL can be provided in urlStr, in which case it is resolved relative to the baseURL of the Client.
+// If specified, the value pointed to by body is JSON encoded and included as the request body.
+func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Request, error) {
+	rel, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+	// Relative URLs should be specified without a preceding slash since baseURL will have the trailing slash
+	rel.Path = strings.TrimLeft(rel.Path, "/")
+
+	u := c.BaseURL.ResolveReference(rel)
+
+	var buf io.ReadWriter
+	if body != nil {
+		buf = new(bytes.Buffer)
+		err = json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set request Headers
+	req.Header.Add("x-api-key", c.Config.APIKey)
+	req.Header.Add("User-Agent", c.UserAgent)
+	req.Header.Add("Content-Type", mediaType)
+	req.Header.Add("Accept", mediaType)
+
+	return req, nil
+}
+
+// Do sends an API request and returns the API response.
+// The API response is JSON decoded and stored in the value pointed to by v, or returned as an error if an API error has occurred.
+func (c *Client) Do(req *http.Request, v interface{}) (*Response, error) {
+	httpResp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Print(httpResp)
+
+	err = CheckResponse(httpResp)
+	if err != nil {
+		return nil, err
+	}
+	if v != nil {
+		// Open a NewDecoder and defer closing the reader only if there is a provided interface to decode to
+		defer httpResp.Body.Close()
+		err = json.NewDecoder(httpResp.Body).Decode(v)
+		if err != nil {
+			return nil, err
+		}
+	}
+	resp := newResponse(httpResp)
+	return resp, err
+}
+
+// CheckResponse checks the Paperpspace API response for a non-2xx status code.
+func CheckResponse(r *http.Response) error {
+	if c := r.StatusCode; 200 <= c && c <= 299 {
+		return nil
+	}
+
+	err := fmt.Errorf("Request failed. Please analyze the request body for more details. Status code: %d", r.StatusCode)
+	return err
+}
+
+// newResponse creates a new Paperspace API Response for http.Response
+func newResponse(r *http.Response) *Response {
+	response := Response{Response: r}
+	return &response
+}
